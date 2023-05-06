cd1a265ecf904c1e9d411e5957c727e1716a75cb
joowy012 <132753456+joowy012@users.noreply.github.com>
2023-05-06 21:09:54 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+let nav = document.getElementById('nav');
+let opened = false;
+let prevH = nav.style.height;
+nav.addEventListener('click', function() {
+    abrir();
+});
+let hamb = document.getElementById('hamb');
+hamb.addEventListener('click', function() {
+    abrir();
+})
+
+function abrir() {
+    if (!opened) {
+        hamb.style.display = 'none';
+        console.log('abierto');
+        nav.style.height = "100vh";
+        console.log(nav.children);
+        for (let i = 0; i < nav.children.length; i++) {
+            nav.children[i].children[0].style.display = 'block';
+        }
+        opened = true;
+    } else {
+        hamb.style.display = 'block';
+        for (let i = 0; i < nav.children.length; i++) {
+            nav.children[i].children[0].style.display = 'none';
+        }
+        console.log('cerrado');
+        nav.style.height = prevH;
+        opened = false;
+    }
+    return
+};
